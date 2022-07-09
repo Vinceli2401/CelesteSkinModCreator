31df7acd85501a84a4569c8bf171ea0778c16562
VancyLee <62593369+VancyLee@users.noreply.github.com>
2022-07-10 07:48:43 +0800

    Update CelesteSkinCreator.py
    Allow full user input

--- CelesteSkinCreator.py
+++ CelesteSkinCreator.py
@@ -4,14 +4,24 @@ import shutil
 from distutils.dir_util import copy_tree
 import glob
 from PIL import Image
+from PIL import ImageColor
 import numpy as np
 #install all the libraries before using the program!!!
 
 skinName = input("Please input your skin name:")
 authorName = input("Please input the author name:")
-dash0 = "FFAABB" #hair color for the dashes, alter at own use, hexadecimal color code
-dash1 = "EE7788"
-dash2 = "BB5577"
+dash0 = input("Please input the hair color for zero dash:") #hair color for the dashes, alter at own use, hexadecimal color code
+dash1 = input("Please input the hair color for one dash:")
+dash2 = input("Please input the hair color for two dash:")
+
+shirt = input('Input hex color for shirt: ')
+shirt = ImageColor.getrgb('#' + shirt)
+sleeves = input('Input hex color for sleeves: ')
+sleeves = ImageColor.getrgb('#' + sleeves)
+collar = input('Input hex color for collar: ')
+collar = ImageColor.getrgb('#' + collar)
+trousers = input('Input hex color for trousers: ')
+trousers = ImageColor.getrgb('#' + trousers)
 
 from_directory = "./SkinBase"
 to_directory = skinName
@@ -66,6 +76,8 @@ shutil.rmtree('./'+skinName+'/Graphics/Atlases/Gameplay/Author')
 
 images=glob.glob('./'+skinName+'/Graphics/Atlases/Gameplay/'+authorName+'/'+skinName+'/characters/**/*.png')
 
+
+
 for image in images:
     img = Image.open(image)
     width = img.size[0]
@@ -74,11 +86,11 @@ for image in images:
         for j in range(0,height):
             data = img.getpixel((i,j))
             if (data[0]==91 and data[1]==110 and data[2]==225): #shirt
-                img.putpixel((i,j),(255, 170, 187)) #rgb color code for color you want to change
+                img.putpixel((i,j),shirt) #rgb color code for color you want to change
             if (data[0]==63 and data[1]==63 and data[2]==116): #sleeves
-                img.putpixel((i,j),(255, 136, 153))
+                img.putpixel((i,j),sleeves)
             if (data[0] == 69 and data[1] == 40 and data[2] == 60): #collar
-                img.putpixel((i, j), (136, 68, 85))
+                img.putpixel((i, j),collar)
             if (data[0] == 135 and data[1] == 55 and data[2] == 36): #trousers
-                img.putpixel((i, j), (187, 85, 119))
+                img.putpixel((i, j),trousers)
     img.save(image)
